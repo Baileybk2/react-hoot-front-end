a0fd7a5f1f3b7454d701547b76df77ce61a2476d
BaileyBeck <baileybk2@gmail.com>
2024-07-26 11:40:45 -0400

    created add hoot functionality and new hoot shows up in hoot index page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -16,6 +16,9 @@ const NavBar = ({ handleSignout }) => {
               <Link to="/hoots">HOOTS</Link>
             </li>
             <li>
+              <Link to="/hoots/new">NEW HOOT</Link>
+            </li>
+            <li>
               <Link to="" onClick={handleSignout}>
                 SIGN OUT
               </Link>
